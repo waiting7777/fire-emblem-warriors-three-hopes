2d4594bb92c04f877769d78d73128a8579e8207f
Wayne <waiting0819@msn.com>
2022-06-21 16:46:02 +0800

    update characters assets

--- components/few/Sidebar.tsx
+++ components/few/Sidebar.tsx
@@ -1,24 +1,28 @@
 import { faHome, faUserGroup, faFeatherPointed } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import ActiveLink from '../ActiveLink'
+import Image from 'next/image'
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col w-full px-4 py-3 text-lg gap-1">
+    <div className="flex flex-col w-full px-2 py-3 text-lg gap-1 border-r border-gray-200">
+      <div className='bg-black mb-3'>
+        <Image src="/images/few/common_logo.webp" width="263" height="124" layout="responsive" alt="logo" />
+      </div>
       <ActiveLink href="/few" activeClassName='text-red-main outline-l-4 outline-red-main'>
-        <div className='flex items-center gap-2 px-2 h-8 border-l-4 border-transparent cursor-pointer hover:bg-red-main hover:text-white'>
+        <div className='flex items-center gap-2 px-3 h-8 border-l-4 border-transparent cursor-pointer hover:bg-red-main hover:text-white'>
           <FontAwesomeIcon className='w-4 h-4' icon={faHome} />
-          <span>Home</span>
+          <span>首頁</span>
         </div>
       </ActiveLink>
       <ActiveLink href="/few/characters" activeClassName='text-red-main border-l-4 border-red-main'>
-        <div className='flex items-center gap-2 px-2 h-8 border-l-4 border-transparent cursor-pointer hover:bg-red-main hover:text-white'>
+        <div className='flex items-center gap-2 px-3 h-8 border-l-4 border-transparent cursor-pointer hover:bg-red-main hover:text-white'>
           <FontAwesomeIcon className='w-4 h-4' icon={faUserGroup} />
-          <span>Characters</span>
+          <span>角色一覽</span>
         </div>
       </ActiveLink>
       <ActiveLink href="/few/classes" activeClassName='text-red-main border-l-4 border-red-main'>
-        <div className='flex items-center gap-2 px-2 h-8 border-l-4 border-transparent cursor-pointer hover:bg-red-main hover:text-white'>
+        <div className='flex items-center gap-2 px-3 h-8 border-l-4 border-transparent cursor-pointer hover:bg-red-main hover:text-white'>
           <FontAwesomeIcon className='w-4 h-4' icon={faFeatherPointed} />
           <span>Classes</span>
         </div>
--- pages/few/characters.tsx
+++ pages/few/characters.tsx
@@ -1,10 +1,16 @@
 import Sidebar from "../../components/few/Sidebar"
+import Seperator from "../../components/Seperator"
+import Image from "next/image"
 
 const Characters = () => {
   return (
     <div className="mx-auto min-h-screen grid grid-cols-[250px_1fr]">
       <Sidebar />
-      <div>charecter</div>
+      <div className="py-8 px-12">
+        <div className="text-4xl">角色一覽</div>
+        <Seperator />
+        <Image src="/images/few/shez.webp" alt="shez" width="200" height="200" />
+      </div>
     </div>
   )
 }
